d01f9ef1890c48e27740f121c6a948edbfc94c00
Vladimir Railean <valdimir.r@travod.com>
2018-11-16 11:32:55 +0200

    Updates

---
+++ precache-manifest.9a22250814442bc0d99f95963c1f7cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/polyglot_ui/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "be40b547a4bfb62894af",
+    "url": "/polyglot_ui/static/js/runtime~main.be40b547.js"
+  },
+  {
+    "revision": "d24d92f374237f781560",
+    "url": "/polyglot_ui/static/js/main.d24d92f3.chunk.js"
+  },
+  {
+    "revision": "205750fa5e3e88653cc2",
+    "url": "/polyglot_ui/static/js/1.205750fa.chunk.js"
+  },
+  {
+    "revision": "d24d92f374237f781560",
+    "url": "/polyglot_ui/static/css/main.c7e78bad.chunk.css"
+  },
+  {
+    "revision": "bbbbd2cbf16dc598e52fb4dc37f5e5cd",
+    "url": "/polyglot_ui/index.html"
+  }
+];
